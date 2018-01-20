af6313bc83cd173ccebd70c3d65bdb5f180198e7
Douglas Couto <DCouto@rightpoint.com>
2018-01-19 19:48:45 -0500

    latest updates.  got the app to generate the solution and skip the obj and bin folders.  a bunch of things got renamed because I went back to the original project I forked.

--- app/index.js
+++ app/index.js
@@ -1,21 +1,113 @@
 'use strict';
 
 const Generator = require('yeoman-generator');
+var yosay = require('yosay');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var fs = require('fs');
+var _ = require('lodash');
+var chalk = require('chalk');
 
 module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
     }
 
-    initializing() {}
+    initializing() {
+        this.config.set("solutionName", "Sample");
+        this.config.set("solutionShortName", "Sample");
+    }
 
     prompting() {
-        this.log("Welcome to to the Sitecore Solution Generator!");
+        let autoSolutionName = this.config.get("autoSolutionName");
+        let autoSolutionShortName = this.config.get("autoSolutionShortName");
+        if(autoSolutionName && autoSolutionShortName){
+            this.config.set("solutionName", autoSolutionName);
+            this.config.set("solutionShortName", autoSolutionShortName);
+            this.config.save();
+            return;
+        }
+
+        this.log(yosay('Bem-vindo ao gerador de solution .NET genérica com DDD!'))
+
+        var generator = this;
+        return this.prompt([{
+            type: "input",
+            name: "solutionName",
+            message: "Nome completo da solution - define o namespace",
+            default: this.config.get("solutionName") || "Sample.Project"
+        }, {
+            when: function(answers) {
+                var nameParts = answers.solutionName.split(".");
+                var multiNameParts = nameParts.length > 1;
+                generator.tempAnswers = multiNameParts ?
+                    _.last(nameParts) :
+                    answers.solutionName;
+                return multiNameParts;
+            },
+            type: "input",
+            name: "solutionShortName",
+            message: "Nome simplificado da solution",
+            default: this.tempAnswers
+        }]).then((answers) => {
+            this.config.set("solutionName", answers.solutionName);
+            this.config.set("solutionShortName", answers.solutionShortName || this.tempAnswers);
+            this.config.save();
+        });
     }
 
     configuring() {}
 
     writing() {
+        template: {
+            this.fs.copy(
+                this.templatePath(),
+                this.templatePath("temp"), {
+                    process: function(content) {
+                        var namespaceRegEx = new RegExp(/SolutionNamespace/, 'g')
+                        var nameRegEx = new RegExp(/SolutionName/, 'g')
+                        return content.toString()
+                            .replace(namespaceRegEx, '<%= solutionName %>')
+                            .replace(nameRegEx, '<%= solutionShortName %>');
+                    }
+                }
+            );
+
+            var solutionName = this.config.get("solutionName");
+            var solutionShortName = this.config.get("solutionShortName");
+            this.registerTransformStream(rename(function(path) {
+                path.basename = path.basename.replace(/(SolutionNamespace)/g, solutionName);
+                path.dirname = path.dirname.replace(/(SolutionNamespace)/g, solutionName);
+
+                path.basename = path.basename.replace(/(SolutionName)/g, solutionShortName);
+                path.dirname = path.dirname.replace(/(SolutionName)/g, solutionShortName);
+            }));
+
+            // this.fs.delete(this.templatePath('temp/**/obj/**/*.*'));
+            // this.fs.delete(this.templatePath('temp/**/bin/**/*.*'));
+
+            // this.fs.copyTpl(
+            //     this.templatePath("temp"),
+            //     this.destinationPath(), {
+            //         solutionName: this.config.get("solutionName"),
+            //         solutionShortName: this.config.get("solutionShortName"),
+            //     }
+            // );
+
+            this.fs.copyTpl(
+                [
+                    this.templatePath('temp'),
+                    '!**/obj/**/*.*',
+                    '!**/bin/**/*.*'
+                ],
+                this.destinationPath(), {
+                    solutionName: this.config.get("solutionName"),
+                    solutionShortName: this.config.get("solutionShortName"),
+                }
+            );
+
+            this.fs.delete(this.templatePath("temp/**/*.*"));
+        }
     }
 
     conflicts() {}
--- app/templates/SolutionNamespace.Web/Properties/AssemblyInfo.cs
+++ app/templates/SolutionNamespace.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SitecoreSolution.Web")]
+[assembly: AssemblyTitle("SolutionNamespace.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SitecoreSolution.Web")]
+[assembly: AssemblyProduct("SolutionNamespace.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("78e1eae0-7237-49fa-a535-884b52adf95d")]
+[assembly: Guid("e6fde417-aa37-4723-bdd9-904f82f34446")]
 
 // Version information for an assembly consists of the following four values:
 //
